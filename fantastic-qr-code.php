af1b3d8ea96ae23dbb0c91b52c5351d0acfb98a2
tarikul Islam <tarikul47@gmail.com>
2024-01-27 12:47:53 +0600

    first commit

--- fantastic-qr-code.php
+++ fantastic-qr-code.php
@@ -9,26 +9,26 @@
  */
 
 class FQC_Qr_Code {
-    public function __construct() {
-        add_action('init', array($this, 'init'));
-    }
+	public function __construct() {
+		add_action( 'init', array( $this, 'init' ) );
+	}
 
-    public function init() {
-        add_filter('the_content', array($this, 'add_qr_code'));
-    }
+	public function init() {
+		add_filter( 'the_content', array( $this, 'add_qr_code' ) );
+	}
 
-    public function add_qr_code($content) {
-        $current_link = esc_url(get_permalink());
-        $title = get_the_title();
-        $custom_content = '<div style="border: 1px solid #ddd; padding: 10px; margin: 20px 0;">';
-        // $custom_content .= '<img src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=' . $current_link . '" alt="'.$title.'" />';
-        // $custom_content .="<img src='https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl={$current_link}' alt='{$title}' />";
-        $custom_content .="<img src='https://api.qrserver.com/v1/create-qr-code/?color=ff0000&size=150x150&data={$current_link}' alt='{$title}' />";
-        $custom_content .= '</div>';
-        
-        $content .= $custom_content;
-        return $content;
-    }
+	public function add_qr_code( $content ) {
+		$current_link   = esc_url( get_permalink() );
+		$title          = get_the_title();
+		$custom_content = '<div style="border: 1px solid #ddd; padding: 10px; margin: 20px 0;">';
+		// $custom_content .= '<img src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=' . $current_link . '" alt="'.$title.'" />';
+		// $custom_content .="<img src='https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl={$current_link}' alt='{$title}' />";
+		$custom_content .= "<img src='https://api.qrserver.com/v1/create-qr-code/?color=ff0000&size=150x150&data={$current_link}' alt='{$title}' />";
+		$custom_content .= '</div>';
+
+		$content .= $custom_content;
+		return $content;
+	}
 }
 
 new FQC_Qr_Code();
---
+++ note.md
@@ -0,0 +1,1 @@
+* git checkout master -b feature/coding-standards
